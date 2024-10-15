73bb1be423faac47323b83b59df9d5d89314a712
JocelynMi10 <jmmiranda@cenfotec.ac.cr>
2024-10-14 19:53:17 -0600

    Creación del Menú de Categoria: Agregando el CRUD   Crear categoría, Editar categoría,  Eliminar categoría, Consultar lista de categorías, Volver al menú principal, Salir

---
+++ cmake-build-debug/CMakeFiles/DataBuildersPIM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DataBuildersPIM.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
@@ -28,7 +29,7 @@ void menuPrincipal() {
                 menuProducto();
             break;
             case 2:
-                menuCategoria();
+               menuCategoria();
             break;
             case 3:
                 menuBusqueda();
@@ -40,6 +41,69 @@ void menuPrincipal() {
             default:
                 cout << "Opcion invalida, intentelo de nuevo." << endl;
             break;
+        }
+    }
+
+}
+
+
+void menuCategoria() {
+    int option;
+    bool exit = false;
+
+    while (!exit) {
+        cout << "===========================================" << endl;
+        cout << "Categorías:" << endl;
+        cout << "===========================================" << endl;
+        cout << "1. Crear categoría" << endl;
+        cout << "2. Editar categoría" << endl;
+        cout << "3. Eliminar categoría" << endl;
+        cout << "4. Consultar lista de categorías" << endl;
+        cout << "5. Volver al menú principal" << endl;
+        cout << "6. Salir" << endl;
+        cout << "=========================================" << endl;
+        cout << "Seleccione una opción (1-6): ";
+
+        try {
+            cin >> option;
+
+            if (cin.fail()) {
+                throw invalid_argument("Entrada inválida. Por favor, ingrese un número.");
+            }
+
+            switch (option) {
+                case 1:
+                    cout << "Crear categoría seleccionada." << endl;
+
+                break;
+                case 2:
+                    cout << "Modificar categoría seleccionada." << endl;
+
+                break;
+                case 3:
+                    cout << "Eliminar categoría seleccionada." << endl;
+
+                break;
+                case 4:
+                    cout << "Consultar lista de categorías seleccionada." << endl;
+
+                break;
+                case 5:
+                    cout << "Volviendo al menú principal..." << endl;
+                exit = true;
+                break;
+                case 6:
+                    cout << "Saliendo del sistema..." << endl;
+                exit = true;
+                break;
+                default:
+                    cout << "Opción inválida, inténtelo de nuevo." << endl;
+                break;
+            }
+        } catch (const invalid_argument &e) {
+            cout << e.what() << endl;
+            cin.clear();  // Limpia el error del flujo
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Descarta el resto de la entrada
         }
     }
 }
