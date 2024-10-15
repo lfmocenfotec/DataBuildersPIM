744998da4a24f1bbb92db7c41cd53e8cf5639cd9
imolinaa <imolinaa@ucenfotec.ac.cr>
2024-10-14 19:14:31 -0600

    menu principal
    primera parte del menu

--- main.cpp
+++ main.cpp
@@ -1,3 +1,45 @@
 //
-// Created by HP on 14/10/2024.
+// Created by NachoM on 14/10/2024.
 //
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void menuPrincipal() {
+    int option;
+    bool exit = false;
+
+    while (!exit) {
+        cout << "===========================================" << endl;
+        cout << "Administrador de información de productos" << endl;
+        cout << "==========================================" << endl;
+        cout << "1. Administrar productos" << endl;
+        cout << "2. Administrar categorias" << endl;
+        cout << "3. Buscar y filtrar productos" << endl;
+        cout << "4. Salir" << endl;
+        cout << "=========================================" << endl;
+        cout << "Seleccione una opcion (1-4): ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                menuProducto();
+            break;
+            case 2:
+                menuCategoria();
+            break;
+            case 3:
+                menuBusqueda();
+            break;
+            case 4:
+                mensajeSalida();
+            exit = true;
+            break;
+            default:
+                cout << "Opcion invalida, intentelo de nuevo." << endl;
+            break;
+        }
+    }
+}
