d7f4a8e04ca3fe76712a24d038f51adc10b65450
JocelynMi10 <jmmiranda@cenfotec.ac.cr>
2024-11-04 22:22:53 -0600

    archivos basura

--- cmake-build-debug/CMakeFiles/DataBuildersPIM.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/DataBuildersPIM.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/guill/Documents/DataBuildersPIM/main.cpp" "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj" "gcc" "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj.d"
+  "C:/Users/jocel/OneDrive/Documentos/DataBuildersPIM/main.cpp" "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj" "gcc" "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
