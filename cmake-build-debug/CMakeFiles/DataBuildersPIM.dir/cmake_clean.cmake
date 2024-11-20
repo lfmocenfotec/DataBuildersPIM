055b9f3bd7f4fb946a9549cd06602124ba2b06a9
JocelynMi10 <jmmiranda@cenfotec.ac.cr>
2024-11-19 22:08:20 -0600

    archivos basura

--- cmake-build-debug/CMakeFiles/DataBuildersPIM.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/DataBuildersPIM.dir/cmake_clean.cmake
@@ -5,8 +5,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/DataBuildersPIM.dir/Producto.cpp.obj.d"
   "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj"
   "CMakeFiles/DataBuildersPIM.dir/main.cpp.obj.d"
-  "CMakeFiles/DataBuildersPIM.dir/sqlite/sqlite3.c.obj"
-  "CMakeFiles/DataBuildersPIM.dir/sqlite/sqlite3.c.obj.d"
   "DataBuildersPIM.exe"
   "DataBuildersPIM.exe.manifest"
   "DataBuildersPIM.pdb"
@@ -14,6 +12,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
+foreach(lang CXX)
   include(CMakeFiles/DataBuildersPIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
